c0acae445a26cad84bf8ed05efe8682f3c398d15
Vinay kumar <100996400+Vinaykumarhub@users.noreply.github.com>
2024-12-02 11:28:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        // code here
+        unordered_map<int,int>map;//intially map is empty
+        //             v   f
+        int n=arr.size();//size
+        int i;//index
+        for(i=0;i<n;i++){
+            map[arr[i]]++;
+        }
+        vector<int>res;
+        for(auto k:map){
+            if(k.second>1){
+                res.push_back(k.first);
+            }
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.findDuplicates(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no duplicates are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
